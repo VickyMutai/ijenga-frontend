c37e132d443c72ade4938aa941e8cbd99b0cfaa9
Bilha <bilhamwengi34@gmail.com>
2025-02-13 12:15:26 +0300

    Changed background for landing page

---
+++ src/components/Particles.tsx
@@ -0,0 +1,105 @@
+     
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
+import { loadSlim } from "@tsparticles/slim";
+
+
+
+interface ParticlesComponentProps {
+  id: string;
+}
+
+const ParticlesComponent = (props: ParticlesComponentProps) => {
+
+  const [, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+//   const particlesLoaded = (container: any) => {
+//     console.log(container);
+//   };
+
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#1D3557",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: '',
+          },
+        },
+        modes: {
+          push: {
+            distance: 200,
+            duration: 15,
+          },
+          grab: {
+            distance: 150,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#FFFFFF",
+        },
+        links: {
+          color: "#FFFFFF",
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        move: {
+          direction: "none" as const,
+          enable: true,
+          outModes: {
+            default: "bounce" as const,
+          },
+          random: true,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 150,
+        },
+        opacity: {
+          value: 1.0,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+
+  return <Particles id={props.id}  options={options} />; 
+};
+
+export default ParticlesComponent;
+
+//init={particlesLoaded}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,14 @@
 import { FaRegBuilding } from "react-icons/fa";
 import { UserRound, UserRoundPlus } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import ParticlesComponent from "../components/Particles";
 
 const LandingPage = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <div className="h-screen bg-gradient-to-br from-[#1D3557] to-[#1D3557]/90 p-4 lg:p-8 flex items-center justify-center">
+      <ParticlesComponent id="particles" />
+      <div className="h-screen p-4 lg:p-8 flex items-center justify-center">
         <main className="flex flex-col-reverse lg:flex-row items-center justify-center">
           {/* Left section */}
           <div className="text-center lg:text-left lg:w-1/2 px-4">
@@ -37,8 +39,8 @@ const LandingPage = () => {
 
           {/* Right section */}
           <div className="lg:w-1/2 flex justify-center mb-8 lg:mb-0">
-            <div className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] bg-white/10 backdrop-blur-lg rounded-full flex items-center justify-center">
-              <FaRegBuilding className="text-[#2ECC71] text-8xl md:text-9xl transform transition-all duration-500 hover:scale-105 animate-float" />
+            <div className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] bg-white/5 backdrop-blur-sm rounded-full flex items-center justify-center">
+              <FaRegBuilding className="text-[#2ECC71] text-8xl md:text-9xl" />
             </div>
           </div>
         </main>
