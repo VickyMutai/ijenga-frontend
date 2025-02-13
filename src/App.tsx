c4c0e5e625f59ab7054806ec1250b680ec72e524
Bilha <bilhamwengi34@gmail.com>
2025-02-13 10:34:06 +0300

    Login, Signup and Forgot Password page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
 
@@ -7,6 +10,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="forgot-password" element={<ForgotPassword />}/>
       </Routes>
     </Router>
   )
